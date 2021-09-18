ba9606596e5390830e61fb1f06859827c6886be3
Andre Pato <anpato1994@gmail.com>
2021-09-18 03:10:49 -0400

    update endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://messenger-backend-sei.herokuapp.com/'
+    ? 'https://messenger-backend-sei.herokuapp.com'
     : 'http://localhost:3001'
 
 const Client = axios.create({ baseURL: BASE_URL })
