6723bcf13ea63818fa97e32cb1ab6af37bdec105
Andre Pato <anpato1994@gmail.com>
2021-09-18 03:34:43 -0400

    fix spacing

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueSocketIO from 'vue-socket.io'
 import socketio from 'socket.io-client'
 import { BASE_URL } from './services/api'
 Vue.config.productionTip = false
+
 const socket = socketio(
   process.env.NODE_ENV === 'production'
     ? BASE_URL.replace('https', 'wss')
